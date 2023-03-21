e11f42c228ec0794aa0d3eab1d88074a7713841d
Aakash Shrestha <jackaakash4@gmail.com>
2023-03-21 13:53:02 +0545

    Learning useReducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import 'tachyons'
 import HookIntervalCounter from './useEffectHook/HookIntervalCounter';
 import DataFetching from './useEffectHook/DataFetching';
+import CounterOne from './useReducerHook/CounterOne';
 
 function App() {
   
   return (
    <div className=''>
-    <DataFetching />
+    <CounterOne />
    </div>
   )
 }
